82585476412dc639bb6b43b12d7adbfeb8246ce4
taywong00 <twong7@stuy.edu>
2019-02-23 20:35:52 -0500

    mockup v1

---
+++ siteseer/siteseer/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  siteseer
+//
+//  Created by Taylor Wong on 2/23/19.
+//  Copyright © 2019 Taylor Wong. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
